41e34a9807c56591535a45625fcf96f864c1628e
gauseen <gauseen@163.com>
2018-12-03 18:33:06 +0800

    update 目录结构

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/**
+ * @Author: gauseen 
+ * @Date: 2018-04-27 11:22:14 
+ * @Last Modified by: gauseen
+ * @Last Modified time: 2018-12-03 18:32:28
+ */
+
+import configENV from './configENV'
+import debounce from './debounce'
+import deepCopy from './deepCopy'
+import forbidBodyScroll from './forbidBodyScroll'
+import timejs from './timejs'
+import getFontSize from './getFontSize'
+import typer from './typer'
+import isEmpty from './isEmpty'
+import mergerOfSimilarItems from './mergerOfSimilarItems'
+import obj2Params from './obj2Params'
+import parseUrl from './parseUrl'
+import regTips from './regTips'
+import setTitle from './setTitle'
+import storage from './storage'
+import threshold from './threshold'
+import throttle from './throttle'
+import isMobile from './isMobile'
+import viewportSize from './viewportSize'
+
+export {
+	configENV,
+	debounce,
+	deepCopy,
+	forbidBodyScroll,
+	getFontSize,
+	typer,
+	timejs,
+	isEmpty,
+	mergerOfSimilarItems,
+	obj2Params,
+	parseUrl,
+	regTips,
+	setTitle,
+	storage,
+	threshold,
+	throttle,
+	isMobile,
+	viewportSize,
+}
